ffe0c0b0ec974253746191a67446f802f5d66921
Ilanklim <142821433+Ilanklim@users.noreply.github.com>
2025-08-13 11:43:41 -0700

    Update health.ts

--- api/health.ts
+++ api/health.ts
@@ -1,5 +1,3 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
